42f487cc9f5a492e7d216c3aadb36d7b8cdfc710
Vinh Tran <vinh_tran@epam.com>
2021-11-26 16:32:14 +0700

    Refactor code & replace fullName logic with getter & setter

--- task2/func.js
+++ task2/func.js
@@ -1,23 +1,28 @@
 function NamedOne(firstName, lastName) {
     this.firstName = firstName;
     this.lastName = lastName;
-    this.fullName = function (newValue) {
-        if (newValue) {
-            const array = newValue.split(' ');
-            if (array.length === 2) {
-                this.firstName = array[0];
-                this.lastName = array[1];
+
+    Object.defineProperty(this, 'fullName', {
+        get() {
+          return `${this.firstName} ${this.lastName}`;
+        },
+        set(value) {
+            if (value) {
+                const array = value.split(' ');
+                if (array.length === 2) {
+                    this.firstName = array[0];
+                    this.lastName = array[1];
+                }
             }
         }
-        return `${this.firstName} ${this.lastName}`;
-    }
+    });
 }
 
-var	namedOne = new NamedOne("Naomi","Wang");
+const namedOne = new NamedOne("Naomi","Wang");
 namedOne.firstName = "John";
 namedOne.lastName = "Doe";
-namedOne.fullName(); // John Doe
+namedOne.fullName; // John Doe
 
-namedOne.fullName('Bill Smith');
+namedOne.fullName = "Bill Smith";
 namedOne.firstName; // Bill
 namedOne.lastName; // Smith
